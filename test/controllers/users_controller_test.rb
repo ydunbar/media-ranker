3b4ba3d3bfb49559334280b5058014ed9c3ee636
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-14 13:45:11 -0700

    Added tests for Show action for Work and User

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,36 @@
 require "test_helper"
 
 describe UsersController do
+
   describe "index" do
     it "should get index" do
       get "/works"
       must_respond_with :success
     end
   end
+
+  describe "show" do
+    before do
+      @user = User.create(username: "Yoyo")
+    end
+
+    it "will get show for valid ids" do
+      # Arrange
+      valid_user_id = @user.id
+      # Act
+      get "/users/#{valid_user_id}"
+      # Assert
+      must_respond_with :success
+    end
+
+    it "will respond with not_found for invalid ids" do
+      # Arrange
+      invalid_user_id = -1
+      # Act
+      get "/users/#{invalid_user_id}"
+      # Assert
+      must_respond_with :not_found
+    end
+  end
+
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,12 +1,45 @@
 require "test_helper"
 
 describe WorksController do
+
   describe "index" do
     it "should get index" do
       get "/works"
       must_respond_with :success
     end
   end
+
+  describe "show" do
+    before do
+      @work = Work.create(
+        category: "movie", 
+        title: "hello world", 
+        creator: "Yoyo", 
+        year: "2020", 
+        description: "Very nice"
+      )
+    end
+
+    it "will get show for valid ids" do
+      # Arrange
+      valid_work_id = @work.id
+      # Act
+      get "/works/#{valid_work_id}"
+      # Assert
+      must_respond_with :success
+    end
+
+    it "will respond with not_found for invalid ids" do
+      # Arrange
+      invalid_work_id = -1
+      # Act
+      get "/works/#{invalid_work_id}"
+      # Assert
+      must_respond_with :not_found
+    end
+
+  end
+
 end
 
 # must_respond_with :success
