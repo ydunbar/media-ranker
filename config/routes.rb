0c3787f373a1153d91c5d3b512148f4666af0951
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-14 14:19:21 -0700

    Added ERB view for homepage, set root path to homepage, and added homepage controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+
+  root to: "homepages#index"  # root_path
+
   get '/works', to: 'works#index'
   get '/works/:id', to: 'works#show'
 
-
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
+  
   # resources :works
   # resources :users
 end
