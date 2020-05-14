bb14025102338d11ec5949aa83a77bd7e2b6a1b3
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-14 15:23:47 -0700

    Added CSS from class example for application and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   root to: "homepages#index"  # root_path
 
-  get '/works', to: 'works#index'
-  get '/works/:id', to: 'works#show'
+  # get '/works', to: 'works#index'
+  # get '/works/:id', to: 'works#show'
 
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  
-  # resources :works
-  # resources :users
+  # get '/users', to: 'users#index'
+  # get '/users/:id', to: 'users#show'
+
+  resources :works
+  resources :users
 end
