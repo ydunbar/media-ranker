1c8cf131c96f4fb26de78adce050925bfae78067
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-19 16:32:53 -0700

    Added placeholder Work method to show top ten Works by category

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,9 @@
 class Work < ApplicationRecord
+
+    def self.top_ten(category)
+        media = Work.where(category: category)
+        top_media = media # .sort by votes
+        return top_media[0..9]
+    end
+
 end
