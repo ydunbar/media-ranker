c42cdbf5e54acad585c7e07af4279c3fb02bd704
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-19 16:35:05 -0700

    Added functionality for Work's top_ten method to return less than 10 top Works

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,7 +3,11 @@ class Work < ApplicationRecord
     def self.top_ten(category)
         media = Work.where(category: category)
         top_media = media # .sort by votes
-        return top_media[0..9]
+        if top_media.length < 10
+            return top_media.all
+        else
+            return top_media[0..9]
+        end
     end
 
 end
