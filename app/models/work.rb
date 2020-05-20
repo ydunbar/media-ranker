146717cb183402960224e6a0d1475a3a033aa42a
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-19 17:13:32 -0700

    Added placeholder Work spotlight method

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,10 @@
 class Work < ApplicationRecord
 
+    def self.spotlight
+        @works = Work.all
+        top_voted = @works[0] # .max_by votes
+    end
+
     def self.top_ten(category)
         media = Work.where(category: category)
         top_media = media # .sort by votes
