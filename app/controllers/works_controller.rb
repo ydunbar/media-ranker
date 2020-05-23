ea41e7eabd772466e9e5c88279d61cbdf8d0b047
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-23 14:48:18 -0700

    DRYed up Works controller with a controller filter

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,16 +1,11 @@
 class WorksController < ApplicationController
+    before_action :find_work, only: [:show, :edit, :update, :destroy]
+
     def index
         @works = Work.order(:category).all
     end
 
-    def show
-        work_id = params[:id]
-        @work = Work.find_by(id: work_id)
-        if @work.nil?
-            head :not_found
-            return
-        end
-    end
+    def show; end
 
     def new
         @work = Work.new
@@ -20,50 +15,33 @@ class WorksController < ApplicationController
         @work = Work.new(work_params)
         if @work.save
             redirect_to root_path
-            return
         else
             render :new # show new work form again
-            return
         end
     end
 
-    def edit
-        @work = Work.find_by(id: params[:id])
-        if @work.nil?
-        head :not_found
-        return
-        end
-    end
+    def edit; end
 
     def update
-        @work = Work.find_by(id: params[:id])
-        if @work.nil?
-            head :not_found
-            return
-        elsif @work.update(work_params)
+        if @work.update(work_params)
             redirect_to works_path
-            return
         else # save failed :(
             render :edit
-            return
         end
     end
 
     def destroy
-        work_id = params[:id]
-        @work = Work.find_by(id: work_id)
-        if @work.nil?
-            head :not_found
-            return
-        end
         @work.destroy
         redirect_to works_path
-        return
     end
 
     private
 
     def work_params
         return params.require(:work).permit(:category, :title, :creator, :year, :description)
+    end
+
+    def find_work
+        @work = Work.find_by_id(params[:id])
     end
 end
