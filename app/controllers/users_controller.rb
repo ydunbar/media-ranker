25b171623cf315c7eb7948e32235ddeb10760dd9
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-23 15:17:30 -0700

    Cleaned up unused actions in User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,21 +15,6 @@ class UsersController < ApplicationController
         end
     end
 
-    def new
-    end
-
-    def create
-    end
-
-    def edit
-    end
-
-    def update
-    end
-
-    def destroy
-    end
-
     def login_form
         @user = User.new
     end
