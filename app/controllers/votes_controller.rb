bea1af3fa43d287396281bcc26c6098b34d1b5b7
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-24 19:29:37 -0700

    Made Work show View refresh with upvote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,6 +4,7 @@ class VotesController < ApplicationController
         @vote = Vote.create(work_id: params[:id],user_id: session[:user_id])
         if session[:user_id] && @vote.valid?
             @vote.save
+            redirect_back(fallback_location: :back)
         elsif @vote.valid? == false
             flash[:error] = "You have already voted for this work"
             redirect_back(fallback_location: :back)
