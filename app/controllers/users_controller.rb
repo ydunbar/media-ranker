5eb5fc45255f34b1f43da17366be095edc36db4d
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-24 19:48:27 -0700

    Added flash alerts to application View

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,14 +21,14 @@ class UsersController < ApplicationController
 
     def login
         username = params[:user][:username]
-        user = User.find_by(username: username)
-        if user
-          session[:user_id] = user.id
-          flash[:success] = "Successfully logged in as returning user #{username}"
+        @user = User.find_by(username: username)
+        if @user
+          session[:user_id] = @user.id
+          flash[:success] = "Successfully logged in as existing user #{username}"
         else
-          user = User.create(username: username)
-          session[:user_id] = user.id
-          flash[:success] = "Successfully logged in as new user #{username}"
+          @user = User.create(username: username)
+          session[:user_id] = @user.id
+          flash[:success] = "Successfully created new user #{username} with ID #{@user.id}"
         end
       
         redirect_to root_path
