3b25d554c615d75854e8db7e219be5533707b4f6
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-25 21:29:32 -0700

    Sorted Users in index View by date created

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
     has_many :votes
     
     validates :username, presence: true
+
+    def self.sorted
+        @users = User.all
+        sorted_users = @users.sort_by { |user| user.created_at }
+        return sorted_users
+    end
 end
